040d69c6a247b5303c0d2857eb5610b1bfa59808
SebastianHO <bastianhdezo@gmail.com>
2022-08-10 17:02:31 -0500

    funciones det, post, put, delete usuario

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -20,9 +20,9 @@ class UserAPIView(APIView):
 def user_api_view(request):
     if request.method == 'GET':
         users = User.objects.all()
-        users_serialized = UserSerializer(users, many=True)
+        users_serializer = UserSerializer(users, many=True)
 
-        return Response(data=users_serialized.data)
+        return Response(data=users_serializer.data)
     elif request.method == 'POST':
         print(request.data)
         user_serializer = UserSerializer(data=request.data)
@@ -31,3 +31,25 @@ def user_api_view(request):
             return Response(user_serializer.data)
 
         return Response(user_serializer.errors)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def get_user_detail_view(request, pk=None):
+    if request.method == 'GET':
+        user = User.objects.filter(id=pk).first()
+        user_serializer = UserSerializer(user)
+        return Response(user_serializer.data)
+
+    elif request.method == 'PUT':
+        user = User.objects.filter(id=pk).first()
+        user_serializer = UserSerializer(instance=user, data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data)
+
+        return Response(user_serializer.errors)
+
+    elif request.method == 'DELETE':
+        user = User.objects.filter(id=pk).first()
+        user.delete()
+        return Response({'mensaje': 'Eliminado'})
--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.users.api.api import UserAPIView, user_api_view
+from apps.users.api.api import UserAPIView, user_api_view, get_user_detail_view
 
 urlpatterns = [
-    # path('usuario/', UserAPIView.as_view(), name='usuario_api') # class based
-    path('usuario/', user_api_view, name='usuario_api')  # function based
+    # path('usuario/', UserAPIView.as_view(), name='usuario_api'), # class based
+    path('usuario/', user_api_view, name='usuario_api'),  # function based
+    path('usuario/<int:pk>', get_user_detail_view, name='usuario__detail_api_view')
 ]
